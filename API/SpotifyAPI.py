d6a939328f0c4c06dd70ed506092217bb1c7385e
LiamMcBride <mailmcbride56@gmail.com>
2022-05-17 12:06:01 -0400

    First

---
+++ API/SpotifyAPI.py
@@ -0,0 +1,26 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from SpotifySecret import CLIENT_ID, CLIENT_SECRET, REDIRECT_LINK
+from urllib.request import urlopen
+
+class SpotifyAPI():
+    def __init__(self):
+        self.authorize()
+    
+    def authorize(self):
+        self.scope = "user-read-currently-playing"
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret= CLIENT_SECRET, redirect_uri=REDIRECT_LINK, scope=self.scope))
+    
+    def getCurrentSongData(self):
+        results = self.sp.current_user_playing_track()
+        if str(results) == "None":
+            return "No Song Playing", "None", "None"
+        artist = results["item"]["artists"][0]['name']
+        song = results["item"]['name']
+        photoArt = results["item"]["album"]["images"][0]["url"]
+
+        return song, artist
+
+api = SpotifyAPI()
+
+print(api.getCurrentSongData())
---
+++ API/SpotifySecret.py
@@ -0,0 +1,3 @@
+CLIENT_ID = "99e01b34b75b4e938d9536171f4f5b15"
+CLIENT_SECRET = "d5a8c381245c4bf29cf5f3e2a4ec3b19"
+REDIRECT_LINK = "http://localhost:8888/callback"
