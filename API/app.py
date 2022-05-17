4cb953ee523a0b8a19902895a19bc15a7eb37a9d
LiamMcBride <mailmcbride56@gmail.com>
2022-05-17 12:16:11 -0400

    Second

--- API/app.py
+++ API/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/hello/', methods=['GET', 'POST'])
+def welcome():
     return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
