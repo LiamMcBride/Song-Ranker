edbb61c178b9a79b3c6151e426c349d05b01cafb
LiamMcBride <mailmcbride56@gmail.com>
2022-05-17 13:44:59 -0400

    issues with posting

--- API/app.py
+++ API/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from re import sub
+from flask import Flask, redirect, url_for, request
 from SpotifyAPI import SpotifyAPI
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
@@ -13,5 +15,15 @@ def welcome():
 def current_song():
     return SpotifyAPI().getCurrentSongJson()
 
+@app.route('/submit/<rand>', methods=['POST'])
+@cross_origin
+def submit(rand):
+    submission = request.json
+    f = open("testFile.txt", "a")
+    f.write(rand)
+    f.close()
+
 if __name__ == '__main__':
+    app.config['CORS_HEADERS'] = 'Content-Type'
     app.run(host='0.0.0.0', port=105)
+    app.config['CORS_HEADERS'] = 'Content-Type'
--- Site/app.js
+++ Site/app.js
@@ -10,6 +10,14 @@ function setArt(url){
     document.getElementById("album-art").src = url;
 }
 
+function getTitle(){
+    return document.getElementById("title").value;
+}
+
+function getAuthor(){
+    return document.getElementById("author").value;
+}
+
 function getData(){
     fetch("http://localhost:105/current/").then((response) => response.json()).then(json => {
         let song = json["song"];
@@ -20,6 +28,25 @@ function getData(){
         setArt(url);
     })
 }
+
+let form = document.getElementById("form");
+form.addEventListener('submit', function(e){
+    let rating = document.getElementById("rating").value;
+
+    fetch("http://localhost:105/submit/1234", {
+        method: "POST",
+        body: JSON.stringify({
+            artist: getAuthor(),
+            song: getTitle(),
+            rating: rating
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    }).then(function(response){
+        return response.json()
+    })
+})
 
 getData();
 var interval = window.setInterval(function(){
